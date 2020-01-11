695ed37a1fd0435d14b3e55bc1af6e0b73f1b470
khaled abdessamia <ek_abdessamia@esi.dz>
2020-01-11 15:04:06 +0100

    Add files via upload

---
+++ env.bash
@@ -0,0 +1,6 @@
+export PATH=$PATH:/usr/local/cuda-8.0/bin
+export CUDA_PATH=/usr/local/cuda-8.0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64
+export C_INCLUDE_PATH=/usr/local/cuda-8.0/samples/common/inc
+export CPLUS_INCLUDE_PATH=/usr/local/cuda-8.0/samples/common/inc
+
